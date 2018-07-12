8e10f1bb97ab73edcb63ec1b1ea00c76f69c332c
knightnight <dushanes@buffalo.edu>
2018-07-12 16:02:29 -0400

    user health info contract and DB helper class created

---
+++ app/src/main/java/dushanes/caloriecounterub/healthInfoContract.java
@@ -0,0 +1,35 @@
+package dushanes.caloriecounterub;
+
+public final class healthInfoContract{
+
+    public healthInfoContract() {};
+
+    public static final class healthInfo{
+        public static final String tableName = "healthInfo";
+        public static final String columnId = "id";
+        public static final String columnName = "name";
+        public static final String columnGender = "gender";
+        public static final String columnAge = "age";
+        public static final String columnWeight = "weight";
+        public static final String columnHeight = "height";
+        public static final String columnActivity = "activity";
+        public static final String columnCalories = "calorie";
+        public static final String columnBmi = "BMI";
+    }
+
+    public static final String sqlCreateEntry =
+            "CREATE TABLE " + healthInfo.tableName + " (" +
+                    healthInfo.columnId + " INTEGER," +
+                    healthInfo.columnName + " TEXT," +
+                    healthInfo.columnGender + " TEXT," +
+                    healthInfo.columnAge + " TEXT," +
+                    healthInfo.columnWeight + " TEXT," +
+                    healthInfo.columnHeight + " TEXT," +
+                    healthInfo.columnActivity + " TEXT," +
+                    healthInfo.columnCalories + " TEXT," +
+                    healthInfo.columnBmi + " TEXT)";
+
+    public static final String sqlDeleteTable =
+            "DROP TABLE IF EXISTS " + healthInfo.tableName;
+
+}
