4b5fbaafe1bf62da99edba954ba12a0a208ebcd4
knightnight <dushanes@buffalo.edu>
2018-07-28 16:26:57 -0400

    Implemented recycler view adapter with an example. TODO: Implement for all meals of the day

--- app/src/main/java/dushanes/caloriecounterub/MainMenu.java
+++ app/src/main/java/dushanes/caloriecounterub/MainMenu.java
@@ -1,9 +1,8 @@
 package dushanes.caloriecounterub;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
@@ -11,7 +10,6 @@ public class MainMenu extends AppCompatActivity {
     private RecyclerView mRecyclerViewBreak;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayout;
-    String h[] = {"test"};
 
     user person;
 
@@ -25,16 +23,20 @@ public class MainMenu extends AppCompatActivity {
         TextView textViewCalories = findViewById(R.id.goal_cal);
         textViewCalories.setText(Integer.toString(person.calories));
 
+        initRecyclerView();
+    }
 
-        /*    *****BUG PRESENT*****
+    private void initRecyclerView(){
+        String h[] = {"test", "hi", "good", "help"};
+
         mRecyclerViewBreak = findViewById(R.id.recycleBreak);
+        mAdapter = new recyclerViewAdapter(h, this);
 
         mRecyclerViewBreak.setHasFixedSize(false);
 
         mLayout = new LinearLayoutManager(this);
         mRecyclerViewBreak.setLayoutManager(mLayout);
 
-        mAdapter = new recyclerViewAdapter(h);
-        mRecyclerViewBreak.setAdapter(mAdapter);*/
+        mRecyclerViewBreak.setAdapter(mAdapter);//
     }
 }
--- app/src/main/java/dushanes/caloriecounterub/recyclerViewAdapter.java
+++ app/src/main/java/dushanes/caloriecounterub/recyclerViewAdapter.java
@@ -1,29 +1,42 @@
 package dushanes.caloriecounterub;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class recyclerViewAdapter extends RecyclerView.Adapter<recyclerViewAdapter.viewHolder>{
 
+    private static final String TAG = "recyclerViewAdapter";
     private String[] dataset;
+    private Context context;
 
     public static class viewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
-        public viewHolder(TextView v){
+        public TextView foodNameText;
+        public TextView caloriesText;
+        RelativeLayout layout;
+        public viewHolder(View v){
             super(v);
-            textView = v;
+            foodNameText = v.findViewById(R.id.foodName);
+            caloriesText = v.findViewById(R.id.foodCalories);
+            layout = v.findViewById(R.id.parent_layout);
         }
     }
 
-    public recyclerViewAdapter(String[] dataset) {
-        dataset = dataset;
+    public recyclerViewAdapter(String[] data, Context context) {
+        this.dataset = data;
+        this.context = context;
     }
 
     @Override
     public recyclerViewAdapter.viewHolder onCreateViewHolder(ViewGroup parent, int type){
-        TextView v = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main_menu, parent, false);
+        Log.d(TAG, "onCreateViewHolder has been called");
+        View v =  LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.recycler_view_main_menu, parent, false);
 
         viewHolder vh = new viewHolder(v);
         return vh;
@@ -31,7 +44,8 @@ public class recyclerViewAdapter extends RecyclerView.Adapter<recyclerViewAdapte
 
     @Override
     public void onBindViewHolder(viewHolder holder, int position) {
-        holder.textView.setText(dataset[position]);
+        Log.d(TAG, "onBindViewHolder has been called");
+        holder.foodNameText.setText(dataset[position]);
     }
 
     @Override
